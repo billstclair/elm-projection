7731d449bf2873b92592f5b4d1ff4d199f7379bf
Bill St. Clair <billstclair@gmail.com>
2025-02-27 10:29:32 -0500

    Projection.projectSeer

--- src/Projection.elm
+++ src/Projection.elm
@@ -12,17 +12,17 @@
 ----------------------------------------------------------------------
 
 
-module Projection exposing (project)
+module Projection exposing (project, projectSeer)
 
 {-| Do geometric projection.
 
 Types are defined in `Projection.Types`.
 
-@docs project
+@docs project, projectSeer
 
 -}
 
-import Projection.Types exposing (Eye, Point, Room, Shape, Vector)
+import Projection.Types exposing (Eye, Point, Room, Seer, Shape, Vector)
 
 
 {-| Project a point to one fewer dimensions.
@@ -31,3 +31,14 @@ project : Point -> Eye -> ( Point, Eye )
 project point eye =
     -- TODO
     ( point, eye )
+
+
+{-| Same as `project`, but takes a `Seer` as the second arg.
+-}
+projectSeer : Point -> Seer -> ( Point, Seer )
+projectSeer point seer =
+    let
+        ( p2, eye2 ) =
+            project point seer.eye
+    in
+    ( p2, { seer | eye = eye2 } )
