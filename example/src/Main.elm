16057355aad6112208eca3376b97a92e235bd019
billstclair <billstclair@gmail.com>
2025-03-05 21:23:39 -0500

    Main.projectTo2

--- example/src/Main.elm
+++ example/src/Main.elm
@@ -3,6 +3,78 @@ module Main exposing (main)
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import Projection exposing (project)
+import Projection.Types exposing (Eye, Number, Point, Shape)
+import Projection.Util as Util
+
+
+theEye : Eye
+theEye =
+    { position = [ 0, 10, 0 ]
+    , direction = [ 0, 0, 0 ]
+    , up = [ 0, 0, 1 ]
+    }
+
+
+e : Number
+e =
+    1 / 2
+
+
+cube : Shape
+cube =
+    [ [ -e, -e, -e ]
+    , [ -e, e, -e ]
+    , [ -e, e, e ]
+    , [ -e, -e, e ]
+    , [ -e, -e, -e ]
+
+    --face 2
+    , [ e, -e, -e ]
+    , [ e, e, -e ]
+    , [ -e, e, -e ]
+    , [ -e, -e, -e ]
+
+    --face 3
+    , [ -e, -e, e ]
+    , [ -e, e, e ]
+    , [ -e, e, -e ]
+    , [ -e, -e, -e ]
+
+    --new base
+    , [ e, -e, -e ]
+
+    -- face 4
+    , [ e, e, -e ]
+    , [ e, e, e ]
+    , [ e, -e, e ]
+    , [ e, -e, -e ]
+
+    -- face 5
+    , [ -e, -e, -e ]
+    , [ -e, e, -e ]
+    , [ e, e, -e ]
+    , [ e, -e, -e ]
+
+    -- face 6
+    , [ e, e, -e ]
+    , [ e, e, e ]
+    , [ e, -e, e ]
+    , [ e, -e, -e ]
+    ]
+
+
+projectTo2 : Point -> Eye -> ( Point, Eye )
+projectTo2 point eye =
+    if Util.pointDimension point == 2 then
+        ( point, eye )
+
+    else
+        let
+            ( p2, e2 ) =
+                Projection.project point eye
+        in
+        projectTo2 p2 e2
 
 
 type alias Model =
